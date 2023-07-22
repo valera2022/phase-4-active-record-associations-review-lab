645815cabc98354ccbd2862ddee2a61fa0ce0ce1
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-22 14:16:28 -0400

    commit tree 79bfa8069f10b31a2da6f1bfc3d568cf2be31001

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,8 @@
 class Passenger < ApplicationRecord
+    has_many :rides
+    has_many :taxis, through: :rides
 end
+# Taxi -< Rides >- Passengers`
+
+# This way, a **taxi** can have many **passengers**, and a **passenger** can have
+# many **taxis**!
--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,8 +1,14 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.string "passenger_id"
+      t.string "taxi_id"
 
       t.timestamps
     end
   end
 end
+# Taxi -< Rides >- Passengers`
+
+# This way, a **taxi** can have many **passengers**, and a **passenger** can have
+# many **taxis**!
